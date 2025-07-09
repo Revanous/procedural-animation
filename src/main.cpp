70756b34febc88b4fc576b7ea58297057686281d
Krevous <d1mon4egv@gmail.com>
2025-07-09 12:07:55 +0300

    Got points for snake outline

--- src/main.cpp
+++ src/main.cpp
@@ -19,37 +19,56 @@ void Main::_ready()
 
 void Main::_draw()
 {
-    draw_circle(points[0], circle_radius, point_color, true, -1.0f, true);
-    for (uint8_t i = 1; i < point_count; ++i)
+    draw_circle(visual_joints[0], 10.0f, outline_color, true, -1.0f, true);
+    for (uint8_t i = 0; i < joint_count; ++i)
     {
-        draw_circle(points[i], circle_radius, point_color, true, -1.0f, true);
-        draw_line(points[i - 1], points[i], point_color, -1.0f, true);
+        draw_circle(visual_joints[i + 1], 10.0f, outline_color, true, -1.0f, true);
+        draw_line(visual_joints[i], visual_joints[i + 1], outline_color, 3.0f, true);
+    }
+
+    for (uint8_t i = 0; i < point_count; ++i)
+    {
+        draw_circle(points[i], 5.0f, Color(1.0f, 0.0f, 0.0f, 1.0f), true, -1.0f, true);
     }
 }
 
 void Main::_process(double delta)
 {
-    points[0] = viewport->get_mouse_position();
+    Vector2 front_point = viewport->get_mouse_position();
+    points[point_count - 1] = front_point;
+    visual_joints[0] = front_point;
 
-    for (uint8_t i = 1; i < point_count; ++i)
+    for (uint8_t i = 0; i < joint_count; ++i)
     {
-        Vector2 difference = points[i] - points[i - 1];
-        points[i] = points[i - 1] + difference.normalized() * point_radius;
+        Vector2 difference = joints[i] - front_point;
+        Vector2 direction = difference.normalized() * joint_radius / 2.0f;
+        difference /= 2.0f;
+
+        points[i].x = front_point.x + difference.x + direction.y;
+        points[i].y = front_point.y + difference.y - direction.x;
+        points[point_count - i - 2].x = front_point.x + difference.x - direction.y;
+        points[point_count - i - 2].y = front_point.y + difference.y + direction.x;
+
+        front_point += direction * 2.0f;
+        joints[i] = front_point;
+        visual_joints[i + 1] = front_point;
     }
+    points[joint_count] = joints[joint_count - 1];
+
     queue_redraw();
 }
 
-void Main::_input(const Ref<InputEvent>& p_event)
-{
-    // Ref<InputEventMouse> mouse_input = p_event;
-    // if (mouse_input != nullptr && mouse_input->get_button_mask() == 1)
-    // {
-    //     points[0] = mouse_input->get_position();
-    //     for (uint8_t i = 1; i < point_count; ++i)
-    //     {
-    //         Vector2 difference = points[i] - points[i - 1];
-    //         points[i] = points[i - 1] + difference.normalized() * point_radius;
-    //     }
-    //     queue_redraw();
-    // }
-}
+// void Main::_input(const Ref<InputEvent>& p_event)
+// {
+//     Ref<InputEventMouse> mouse_input = p_event;
+//     if (mouse_input != nullptr && mouse_input->get_button_mask() == 1)
+//     {
+//         points[0] = mouse_input->get_position();
+//         for (uint8_t i = 1; i < point_count; ++i)
+//         {
+//             Vector2 difference = points[i] - points[i - 1];
+//             points[i] = points[i - 1] + difference.normalized() * point_radius;
+//         }
+//         queue_redraw();
+//     }
+// }
--- src/main.h
+++ src/main.h
@@ -5,7 +5,8 @@
 #include <godot_cpp/classes/viewport.hpp>
 #include <godot_cpp/classes/input_event.hpp>
 
-#define MAX_POINTS 5
+#define MAX_JOINTS 5
+#define MAX_POINTS ((MAX_JOINTS + 1) * 2)
 
 namespace godot
 {
@@ -15,16 +16,18 @@ class Main : public Node2D
     GDCLASS(Main, Node2D);
 
 private:
-    const Color point_color = Color(0.4f, 0.9f, 0.8f, 1.0f);
-    const float circle_radius = 5.0f;
-    const float point_radius = circle_radius + 55.0f;
+    const uint8_t joint_count = MAX_JOINTS;
+    const uint8_t point_count = MAX_POINTS;
+    const float joint_radius = 100.0f;
+    const Color outline_color = Color(0.4f, 0.9f, 0.8f, 1.0f);
 
-    uint8_t point_count = MAX_POINTS;
     Viewport* viewport;
 
     Vector2 screen_size;
     Vector2 screen_center;
+    Vector2 joints[MAX_JOINTS];
     Vector2 points[MAX_POINTS];
+    Vector2 visual_joints[MAX_JOINTS + 1];
 
 protected:
     static void _bind_methods();
@@ -33,7 +36,7 @@ public:
     void _ready() override;
     void _draw() override;
     void _process(double delta) override;
-    void _input(const Ref<InputEvent>& p_event) override;
+    // void _input(const Ref<InputEvent>& p_event) override;
 };
 
 }
